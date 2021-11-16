879def20c8a2a84aa35c432eea121532153ee1d4
lalomx <lalo0892@gmail.com>
2021-11-16 00:48:27 -0600

    tests: Add option field to tests

--- cornershop-backend-test/lunch/tests/test_api.py
+++ cornershop-backend-test/lunch/tests/test_api.py
@@ -15,6 +15,10 @@ class APITests(TestCase):
             created_by="test",
             updated_by="test",
             name="Menu 1",
+            option_one="Option 1",
+            option_two="Option 2",
+            option_three="Option 3",
+            option_four="Option 4",
             id=MENU_ID,
         )
 
@@ -45,6 +49,10 @@ class APITests(TestCase):
         self.assertEqual("test", menu["created_by"])
         self.assertEqual("test", menu["updated_by"])
         self.assertEqual("Menu 1", menu["name"])
+        self.assertEqual("Option 1", menu["option_one"])
+        self.assertEqual("Option 2", menu["option_two"])
+        self.assertEqual("Option 3", menu["option_three"])
+        self.assertEqual("Option 4", menu["option_four"])
 
     def test_retrieve_menu_not_valid_uuid(self):
         c = Client()
