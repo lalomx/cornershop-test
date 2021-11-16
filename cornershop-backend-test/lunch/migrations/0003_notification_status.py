c0bb1dc0f770fcc5f01c82a226e5bbdb5735c1f8
lalomx <lalo0892@gmail.com>
2021-11-16 12:26:01 -0600

    lint: Black into migrations file

--- cornershop-backend-test/lunch/migrations/0003_notification_status.py
+++ cornershop-backend-test/lunch/migrations/0003_notification_status.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('lunch', '0002_auto_20211116_0643'),
+        ("lunch", "0002_auto_20211116_0643"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='notification',
-            name='status',
-            field=models.CharField(default='P', max_length=1),
+            model_name="notification",
+            name="status",
+            field=models.CharField(default="P", max_length=1),
         ),
     ]
