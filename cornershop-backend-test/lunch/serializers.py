e84c3765f755c90d52c62006d4ca17969dcec9ae
lalomx <lalo0892@gmail.com>
2021-11-16 01:03:50 -0600

    feat: Add order serializer

--- cornershop-backend-test/lunch/serializers.py
+++ cornershop-backend-test/lunch/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework.serializers import ModelSerializer
 
-from lunch.models import Menu
+from lunch.models import Menu, Order
+
+
+class MenuSerializerSlim(ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ["id", "name"]
 
 
 class MenuSerializer(ModelSerializer):
     class Meta:
         model = Menu
+        fields = "__all__"
+
+
+class OrderSerializer(ModelSerializer):
+    menu = MenuSerializerSlim()
+
+    class Meta:
+        model = Order
         fields = "__all__"
--- cornershop-backend-test/lunch/views.py
+++ cornershop-backend-test/lunch/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from rest_framework import mixins, viewsets
 from rest_framework.decorators import action
 
-from lunch.models import Menu
-from lunch.serializers import MenuSerializer
+from lunch.models import Menu, Order
+from lunch.serializers import MenuSerializer, OrderSerializer
 from lunch.tasks import SlackNotification
 
 
@@ -30,3 +30,14 @@ class MenuViewSet(
     @action(detail=True)
     def orders(self, request, pk=None):
         pass
+
+
+class OrderViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    lookup_value_regex = "[0-9a-f]{32}"
+    lookup_field = "id"
