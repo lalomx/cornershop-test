65046d458c8e242d7b28b24b8e09a86aa7b5a567
evelazquez <evelazquez@avionica.com>
2021-11-18 15:35:14 -0600

    feat: Add choose view

--- cornershop-backend-test/lunch/urls.py
+++ cornershop-backend-test/lunch/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 from rest_framework.routers import SimpleRouter
 
-from lunch.views import MenuViewSet, OrderViewSet
+from lunch.views import ChooseView, MenuViewSet, OrderViewSet
 
 router = SimpleRouter(trailing_slash=False)
 router.register(r"menu", MenuViewSet)
@@ -10,6 +10,8 @@ router.register(r"order", OrderViewSet)
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
-    path("/<slug:slug>", TemplateView.as_view(template_name="index.html")),
+    path("choose/<uuid:id>", ChooseView.as_view(), name="choose"),
+    path("thanks", TemplateView.as_view(template_name="thanks.html")),
+    path("out", TemplateView.as_view(template_name="out.html")),
     path("api/", include(router.urls)),
 ]
--- cornershop-backend-test/lunch/views.py
+++ cornershop-backend-test/lunch/views.py
@@ -1,10 +1,65 @@
+from datetime import datetime
+from pytz import timezone
+
+from django.contrib import messages
+from django.views.generic.edit import FormView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
 from rest_framework import generics, mixins, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from lunch.models import Menu, Order
+from lunch.forms import OrderForm
+from lunch.models import Menu, Notification, Order
 from lunch.serializers import MenuSerializer, OrderSerializer
 from lunch.tasks import SlackNotification
+
+
+class ChooseView(FormView):
+    template_name = "choose.html"
+    form_class = OrderForm
+    success_url = "/lunch/thanks"
+
+    def form_valid(self, form, notification_id):
+        form.create_order(notification_id)
+        return super().form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        notification_id = str(kwargs["id"])
+        notification = Notification.objects.get(id=notification_id)
+        
+        if notification.status == "D":
+            return render(request, "thanks.html", { "done": True })
+
+        form = self.get_form_choices(request.POST, notification)
+        if form.is_valid():
+            return self.form_valid(form, notification)
+        else:
+            return self.form_invalid(form)
+
+    def get(self, request, id):
+        clt = timezone('America/Santiago')
+        date = datetime.now().astimezone(clt)
+
+        if date.time().hour >= 11:
+            return HttpResponseRedirect("/lunch/out")
+
+        notification = Notification.objects.get(id=id)
+        form = self.get_form_choices(None, notification)
+        return render(request, self.template_name, { "form": form, "id": id })
+
+    def get_form_choices(self, data, notification):
+        menu = notification.menu
+        form = self.form_class(data)
+        form.fields["option"].choices = [
+            (menu.option_one, menu.option_one),
+            (menu.option_two, menu.option_two),
+            (menu.option_three, menu.option_three),
+            (menu.option_four, menu.option_four),
+        ]
+
+        return form
 
 
 class MenuViewSet(
@@ -12,7 +67,7 @@ class MenuViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
     viewsets.GenericViewSet,
-    generics.UpdateAPIView
+    generics.UpdateAPIView,
 ):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
