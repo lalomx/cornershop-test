a3cae518ee8988c219c7502a3898980fe9754c3f
lalomx <lalo0892@gmail.com>
2021-11-18 01:27:59 -0600

    fix: Update test

--- cornershop-backend-test/lunch/tasks.py
+++ cornershop-backend-test/lunch/tasks.py
@@ -20,6 +20,9 @@ class SlackNotification(app.Task):
         menu = Menu.objects.get(id=menu_id)
         logger.info("Slack notification started")
         for employee in employees:
+            notification = Notification()
+            notification.employee = employee
+            notification.menu = menu
             text = f"""Hi, {employee.first_name}! Here's today's menu!
           > {menu.option_one}
           > {menu.option_two}
@@ -29,13 +32,10 @@ class SlackNotification(app.Task):
           http://localhost:8000/lunch/choose/{notification.id}"""
 
             self.client.chat_postMessage(channel=employee.slack_id, text=text)
-            self.save_notification(menu, employee)
+            self.save_notification(notification)
     
-    def save_notification(self, menu, employee):
-        notification = Notification()
+    def save_notification(self, notification):
         notification.channel_name = 'slack'
-        notification.employee = employee
-        notification.menu = menu
         notification.status = "SENT"
         notification.save()
 
