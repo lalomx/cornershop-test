c6e3bd4913b2ff23c5af49a92d13855dcde3a03d
evelazquez <evelazquez@avionica.com>
2021-11-18 16:12:42 -0600

    fix: Order screen

--- cornershop-backend-test/ui/src/views/Orders.vue
+++ cornershop-backend-test/ui/src/views/Orders.vue
@@ -1,83 +1,71 @@
 <template>
-<div class="container mx-auto flex flex-col">
-  <div class="text-center pb-12">
-      <h1
-        class="
+  <div class="container mx-auto flex flex-col">
+    <div class="text-center pb-12">
+      <h1 class="
           font-bold
           text-3xl
           md:text-4xl
           lg:text-5xl
           font-heading
           text-gray-900
-        "
-      >
+        ">
         Orders
       </h1>
     </div>
-  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-        <table class="min-w-full divide-y divide-gray-200">
-          <thead class="bg-gray-50">
-            <tr>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Name
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Title
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Status
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Role
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-start">
-                  <div class="ml-4">
-                    <div class="text-sm font-medium text-gray-900">
-                      Jane Cooper
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Employee
+                </th>
+                <th scope="col"
+                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Choice
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <template v-if="orders && orders.length">
+                <tr v-for="order in orders" :key="order.id">
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-start">
+                      <div class="ml-4">
+                        <div class="text-sm font-medium text-gray-900">
+                          {{ order.employee.first_name}} {{ order.employee.last_name}}
+                        </div>
+                        <div class="text-sm text-gray-500">
+                          {{ order.employee.email }}
+                        </div>
+                      </div>
                     </div>
-                    <div class="text-sm text-gray-500">
-                      jane.cooper@example.com
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
-                <div class="text-sm text-gray-500">Optimization</div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                  Active
-                </span>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                Admin
-              </td>
-            </tr>
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-start">
-                  <div class="ml-4">
-                    <div class="text-sm font-medium text-gray-900">
-                      No results
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">{{ order.selection }}</div>
+                    <div class="text-sm text-gray-500">{{ order.comments }}</div>
+                  </td>
+                </tr>
+              </template>
+              <tr v-else>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-start">
+                    <div class="ml-4">
+                      <div class="text-sm font-medium text-gray-900">
+                        No results
+                      </div>
                     </div>
                   </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -86,9 +74,14 @@ import api from '../mixins/api.mixin'
 export default {
   name: 'Orders',
   mixins: [api],
+  data () {
+    return {
+      orders: []
+    }
+  },
   async created () {
     const { data: orders } = await this.getOrders()
-    console.log(orders)
+    this.orders = orders
   }
 }
 </script>
