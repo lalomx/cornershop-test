6a34331ddbe4086896a116b9ca040f96dcd7904d
lalomx <lalo0892@gmail.com>
2021-11-18 19:50:45 -0600

    lint: Fix for lint

--- cornershop-backend-test/backend_test/urls.py
+++ cornershop-backend-test/backend_test/urls.py
@@ -20,5 +20,5 @@ from .utils.healthz import healthz
 urlpatterns = [
     path("healthz", healthz, name="healthz"),
     path("lunch/", include("lunch.urls")),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
